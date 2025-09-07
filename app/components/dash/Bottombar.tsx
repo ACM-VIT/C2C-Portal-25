371d6e39a1be788fa37b3a76e9b015d6f9bf391e
Bhanuch2006 <maheshbhanu.chekuri@gmail.com>
2025-09-07 13:19:25 +0530

    logo size change

--- app/components/dash/Bottombar.tsx
+++ app/components/dash/Bottombar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Bottombar() {
   return (
-    <footer className="relative w-full bg-black py-2">
+    <footer className="relative w-full bg-black py-3">
       <div className="relative flex items-center justify-between w-full px-1 sm:px-4">
         
         
@@ -14,7 +14,7 @@ export default function Bottombar() {
             alt="VIT Logo"
             width={150}
             height={70}
-            className="w-[60px] sm:w-[90px] md:w-[120px] lg:w-[140px] h-auto"
+            className="w-[70px] sm:w-[90px] md:w-[120px] lg:w-[140px] h-auto"
           />
         </div>
 
@@ -22,19 +22,19 @@ export default function Bottombar() {
           <Image
             src="/dash/powered.svg"
             alt="Powered by Runpod and ElevenLabs"
-            width={260}
-            height={100}
-            className="w-[120px] sm:w-[160px] md:w-[200px] lg:w-[350px] h-auto"
+            width={300}
+            height={200}
+            className="w-[280px] sm:w-[450px] md:w-[400px] lg:w-[500px] h-auto"
           />
         </div>
-        
+
         <div className="flex-shrink-0">
           <Image
             src="/dash/gravitas.svg"
             alt="Gravitas Logo"
             width={150}
             height={70}
-            className="w-[60px] sm:w-[90px] md:w-[120px] lg:w-[140px] h-auto"
+            className="w-[70px] sm:w-[90px] md:w-[120px] lg:w-[140px] h-auto"
           />
         </div>
       </div>
